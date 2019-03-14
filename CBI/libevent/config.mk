332fd0d83412408552ebcf5ccd878ff2992eb39f
hb <hb@aroma-project.org>
2019-03-13 20:14:27 -0700

    CLEANUP: libevent

---
+++ CBI/libevent/config.mk
@@ -0,0 +1,5 @@
+NAME=libevent
+URL=https://github.com/libevent/libevent/releases
+BUILD_SUFFIX=-stable
+BUILD_TARGET_FILE=.libs/libevent.so
+INSTALL_TARGET_FILE=lib/libevent.so
--- CBI/utils.mk
+++ CBI/utils.mk
@@ -8,8 +8,25 @@ ifndef VERSION
   $(error ERROR: Environment variable 'VERSION' is not set)
 endif
 
-TARBALL=$(NAME)-$(VERSION)$(BUILD_SUFFIX).tar.gz
 
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## DOWNLOADING
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ifndef DOWNLOAD_TARGET
+  ifndef DOWNLOAD_TARGET_FILE
+    DOWNLOAD_TARGET_FILE=configure
+  endif
+  DOWNLOAD_TARGET=$(BUILD_PATH)/$(DOWNLOAD_TARGET_FILE)
+endif
+
+ifndef TARBALL
+  TARBALL=$(NAME)-$(VERSION)$(BUILD_SUFFIX).tar.gz
+endif
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## BUILDING
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ifndef BUILD_HOME
   ifndef TMPDIR
     $(error ERROR: Environment variable 'TMPDIR' is not set)
@@ -25,6 +42,32 @@ ifndef BUILD_PATH
   BUILD_PATH=$(BUILD_HOME)/$(BUILD_NAME)
 endif
 
+ifndef CONFIG_TARGET_FILE
+  CONFIG_TARGET_FILE=config.log
+endif
+
+ifndef CONFIG_TARGET
+  CONFIG_TARGET=$(BUILD_PATH)/$(CONFIG_TARGET_FILE)
+endif
+
+ifndef BUILD_TARGET
+  ifndef BUILD_TARGET_FILE
+    $(error ERROR: Environment variable 'BUILD_TARGET_FILE' is not set)
+  endif
+  BUILD_TARGET=$(BUILD_PATH)/$(BUILD_TARGET_FILE)
+endif
+
+ifndef INSTALL_TARGET
+  ifndef INSTALL_TARGET_FILE
+    $(error ERROR: Environment variable 'INSTALL_TARGET_FILE' is not set)
+  endif
+  INSTALL_TARGET=$(PREFIX)/$(INSTALL_TARGET_FILE)
+endif
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## SOFTWARE
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ifndef SOFTWARE_HOME
   $(error ERROR: Environment variable 'SOFTWARE_HOME' is not set)
 endif
@@ -33,8 +76,34 @@ ifndef PREFIX
   PREFIX=$(SOFTWARE_HOME)/$(NAME)-$(VERSION)
 endif
 
+download_software: $(DOWNLOAD_TARGET)
+
+$(CONFIG_TARGET): $(DOWNLOAD_TARGET)
+	module purge
+	cd $(BUILD_PATH); \
+	./configure --prefix=$(PREFIX)
+
+config_software: $(CONFIG_TARGET)
+
+$(BUILD_TARGET): $(CONFIG_TARGET)
+	module purge;
+	cd $(BUILD_PATH); \
+	make
+
+build_software: $(BUILD_TARGET)
+
+$(INSTALL_TARGET): $(BUILD_TARGET)
+	cd $(BUILD_PATH); \
+	make install
+	ls -la $(PREFIX)
+	@echo "SOFTWARE INSTALLED TO: $(PREFIX)"
+
+install_software: $(INSTALL_TARGET)
 
 
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## MODULE
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ifndef MODULE_HOME
   $(error ERROR: Environment variable 'MODULE_HOME' is not set)
 endif
@@ -50,40 +119,57 @@ endif
 MODULE_NAME_VERSION=$(MODULE_NAME)/$(VERSION)
 
 
+ifndef MODULE_TARGET
+  MODULE_TARGET=$(MODULE_HOME)/$(MODULE_NAME)/$(VERSION).lua
+endif
+
+$(MODULE_TARGET): module/module.lua.tmpl
+	mkdir -p "$(@D)"
+	cp "$<" "$@"
+	module --ignore-cache show $(MODULE_NAME)/$(VERSION)
+	module load $(MODULE_NAME)/$(VERSION)
+	module unload $(MODULE_NAME)/$(VERSION)
+
+install_module: $(MODULE_TARGET)
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## DEBUG
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 debug:
 	@echo "NAME: $(NAME)"
 	@echo "VERSION: $(VERSION)"
-	@echo "TMPDIR*: $(TMPDIR)"
+	@echo "URL: $(URL)"
+	@echo
+	@echo "DOWNLOADING:"
 	@echo "TARBALL*: $(TARBALL)"
-	@echo "SOFTWARE_HOME: $(SOFTWARE_HOME)"
-	@echo "PREFIX: $(PREFIX)"
+	@echo
+	@echo "BUILDING:"
+	@echo "TMPDIR*: $(TMPDIR)"
 	@echo "BUILD_HOME: $(BUILD_HOME)"
-	@echo "BUILD_PATH: $(BUILD_PATH)"
-	@echo "BUILD_SUFFIX*: $(BUILD_SUFFIX)"
 	@echo "BUILD_NAME: $(BUILD_NAME)"
+	@echo "BUILD_SUFFIX*: $(BUILD_SUFFIX)"
+	@echo "BUILD_PATH: $(BUILD_PATH)"
+	@echo "CONFIG_TARGET: $(CONFIG_TARGET)"
+	@echo "BUILD_TARGET: $(BUILD_TARGET)"
+	@echo
+	@echo "MODULES:"
 	@echo "MODULE_HOME: $(MODULE_HOME)"
 	@echo "MODULE_NAME: $(MODULE_NAME)"
 	@echo "MODULE_NAME_VERSION: $(MODULE_NAME_VERSION)"
 	@echo "FULLNAME: $(FULLNAME)"
-	@echo "URL: $(URL)"
+	@echo "MODULE_TARGET: $(MODULE_TARGET)"
+	@echo
+	@echo "INSTALLATION:"
+	@echo "SOFTWARE_HOME: $(SOFTWARE_HOME)"
+	@echo "PREFIX: $(PREFIX)"
+	@echo "INSTALL_TARGET: $(INSTALL_TARGET)"
 
-all: debug install
 
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## EVERYTHING
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 install: install_software install_module
 
-install_software:
-	cd install; \
-	make NAME=$(NAME)
-
-install_module: build_module test_module
-
-$(MODULE_HOME)/$(MODULE_NAME)/$(VERSION).lua: module/module.lua.tmpl
-	mkdir -p "$(@D)"
-	cp "$<" "$@"
-
-build_module: $(MODULE_HOME)/$(MODULE_NAME)/$(VERSION).lua
-
-test_module: $(MODULE_HOME)/$(MODULE_NAME)/$(VERSION).lua
-	module --ignore-cache show $(MODULE_NAME)/$(VERSION)
-	module load $(MODULE_NAME)/$(VERSION)
-	module unload $(MODULE_NAME)/$(VERSION)
+all: debug install

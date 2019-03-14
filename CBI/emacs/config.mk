9363579e1800ac4e30210c01f5b800930e0170fe
hb <hb@aroma-project.org>
2019-03-13 21:18:51 -0700

    emacs

---
+++ CBI/emacs/config.mk
@@ -0,0 +1,6 @@
+NAME=emacs
+VERSION=25.3
+URL=http://gnu.mirrors.hoobly.com/emacs
+CONFIG_OPTS=--with-xpm=no --with-gif=no --with-gnutls=no
+BUILD_TARGET_FILE=src/emacs
+INSTALL_TARGET_FILE=bin/emacs

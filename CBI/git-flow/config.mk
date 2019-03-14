f1e0cdbb9415b8aad75a8424ad91f7cbff11187d
hb <hb@aroma-project.org>
2019-03-13 23:03:27 -0700

    git-flow

---
+++ CBI/git-flow/config.mk
@@ -0,0 +1,8 @@
+NAME=git-flow
+VERSION=1.12.1
+URL=https://github.com/petervanderdoes/gitflow-avh/releases
+DOWNLOAD_TARGET_FILE=AUTHORS
+CONFIG_TARGET_FILE=Makefile.org
+BUILD_NAME=gitflow-avh-$(VERSION)
+BUILD_TARGET_FILE=build-dummy.log
+INSTALL_TARGET_FILE=bin/git-flow

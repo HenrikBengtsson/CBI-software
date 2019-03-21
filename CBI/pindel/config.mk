020563e57ec9ce01e6bd70d8e0839bfecc7702ac
hb <hb@aroma-project.org>
2019-03-21 10:13:18 -0700

    SOFTWARE: pindel 0.2.5b8

---
+++ CBI/pindel/config.mk
@@ -0,0 +1,8 @@
+NAME=pindel
+VERSION=0.2.5b8
+URL=https://www.sanger.ac.uk/science/tools/pindel
+DOWNLOAD_TARGET_FILE=INSTALL
+CONFIG=false
+BUILD_TARGET_FILE=pindel
+INSTALL_TARGET_FILE=pindel
+WARNING="This software is built with rpath, i.e. it needs to be rebuilt if its installation path changed"

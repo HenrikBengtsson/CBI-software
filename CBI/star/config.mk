853d30e5c08e896013ae51cf2aa66ebf1e02e872
hb <hb@aroma-project.org>
2019-03-22 17:42:24 -0700

    SOFTWARE: star/2.7.0e

---
+++ CBI/star/config.mk
@@ -0,0 +1,8 @@
+NAME=STAR
+MODULE_NAME=star
+VERSION=2.7.0e
+URL=https://github.com/alexdobin/STAR
+DOWNLOAD_TARGET_FILE=bin/Linux_x86_64/$(NAME)
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=bin/Linux_x86_64/$(NAME)

de6c18cc0badf87d743569a582c1fce01f5ce23e
hb <hb@aroma-project.org>
2019-04-18 21:24:34 -0700

    Add utils/ software tools

---
+++ utils/bash-startup/config.mk
@@ -0,0 +1,8 @@
+NAME=bash-startup
+VERSION:=0.5.0
+URL=https://github.com/HenrikBengtsson/bash-startup/releases
+DOWNLOAD_TARGET_FILE=bash-startup
+CONFIG=false
+BUILD=false
+SOFTWARE_HOME=$(SOFTWARE_ROOT_CBI)/../utils
+INSTALL_TARGET_FILE=bash-startup
---
+++ utils/utils.mk
@@ -0,0 +1,1 @@
+../CBI/utils.mk

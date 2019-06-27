394b0d7e1eb684dc25470c28d2029c156e3290b1
hb <hb@aroma-project.org>
2019-06-27 13:55:03 -0700

    SOFTWARE: ASCIIGenome 1.15.0

---
+++ CBI/asciigenome/config.mk
@@ -0,0 +1,8 @@
+NAME=ASCIIGenome
+MODULE_NAME=asciigenome
+VERSION:=1.15.0
+URL=https://github.com/dariober/ASCIIGenome
+DOWNLOAD_TARGET_FILE=$(NAME)
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=$(NAME)

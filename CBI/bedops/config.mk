1011b2a14730730e70f00b223208500723e20283
hb <hb@aroma-project.org>
2019-06-27 14:59:52 -0700

    SOFTWARE: bedops 2.4.36

---
+++ CBI/bedops/config.mk
@@ -0,0 +1,8 @@
+NAME=bedops
+VERSION:=2.4.36
+URL=https://bedops.readthedocs.io/en/latest/
+DOWNLOAD_TARGET_FILE=bin/$(NAME)
+CONFIG=false
+BUILD=false
+#BUILD_TARGET_FILE=bin/$(NAME)
+INSTALL_TARGET_FILE=bin/$(NAME)

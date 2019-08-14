7092a9eabe03bcddcbabb043a1a19ac49784b58c
hb <hb@aroma-project.org>
2019-08-14 00:14:53 -0700

    MODULES: bamutil/1.0.14

---
+++ CBI/bamutil/config.mk
@@ -0,0 +1,8 @@
+NAME=bamUtil
+VERSION:=1.0.14
+URL=https://github.com/statgen/bamUtil
+MODULE_NAME=bamutil
+DOWNLOAD_TARGET_FILE=Makefile
+CONFIG=false
+BUILD_TARGET_FILE=bin/bam
+INSTALL_TARGET_FILE=bam

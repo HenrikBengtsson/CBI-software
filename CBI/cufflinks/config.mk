5c48c3ce92055603a19f9b8964928daec012d4e5
hb <hb@aroma-project.org>
2019-09-07 18:42:36 -0700

    Add cufflinks/2.2.1

---
+++ CBI/cufflinks/config.mk
@@ -0,0 +1,7 @@
+NAME=cufflinks
+VERSION:=2.2.1
+URL=http://cole-trapnell-lab.github.io/cufflinks/, https://github.com/alexdobin/STAR
+DOWNLOAD_TARGET_FILE=cufflinks
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=cufflinks

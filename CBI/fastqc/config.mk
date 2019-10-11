0be7dc3142aee08390c1ffe76d493f819bbe18b0
hb <hb@aroma-project.org>
2019-10-11 14:04:49 -0700

    MODULES: fastqc/0.11.8

---
+++ CBI/fastqc/config.mk
@@ -0,0 +1,9 @@
+NAME=FastQC
+MODULE_NAME=fastqc
+VERSION:=0.11.8
+URL=https://www.bioinformatics.babraham.ac.uk/projects/download.html#fastqc
+DOWNLOAD_TARGET_FILE=fastqc
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=fastqc
+

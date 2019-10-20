73d92ea6bbeb27c9212b6c2b927e0b3ff4a7dd7f
hb <hb@aroma-project.org>
2019-10-19 21:38:33 -0700

    MODULE: varscan/2.4.2

---
+++ CBI/varscan/config.mk
@@ -0,0 +1,7 @@
+NAME=VarScan
+VERSION:=2.4.2
+URL=https://dkoboldt.github.io/varscan/
+DOWNLOAD_TARGET_FILE=$(NAME).v$(VERSION).jar
+CONFIG=false
+BUILD_TARGET_FILE=$(NAME).jar
+INSTALL_TARGET_FILE=$(NAME)

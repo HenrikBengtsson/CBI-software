e2152b3269c741ef6f30b6a44b2c4202b725e87f
hb <hb@aroma-project.org>
2019-10-19 22:00:48 -0700

    MODULE: sratoolkit/2.10.0

---
+++ CBI/sratoolkit/config.mk
@@ -0,0 +1,7 @@
+NAME=sratoolkit
+VERSION:=2.10.0
+URL=https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=software, https://ncbi.github.io/sra-tools/
+DOWNLOAD_TARGET_FILE=bin/fastq-dump
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=bin/fastq-dump

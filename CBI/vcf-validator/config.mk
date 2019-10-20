109884ffd039f9fe4fdf7fc583370013a59ac388
hb <hb@aroma-project.org>
2019-10-19 21:13:49 -0700

    MODULE: vcf-validator/0.9.2

---
+++ CBI/vcf-validator/config.mk
@@ -0,0 +1,7 @@
+NAME=vcf-validator
+VERSION:=0.9.2
+URL=https://github.com/EBIvariation/vcf-validator
+DOWNLOAD_TARGET_FILE=vcf_validator_linux
+CONFIG=false
+BUILD_TARGET_FILE=build/vcf_validator
+INSTALL_TARGET_FILE=vcf_validator

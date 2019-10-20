7a38140c80d142bbe6a33f2c515735e2b6cd771f
hb <hb@aroma-project.org>
2019-10-20 16:53:26 -0700

    MODULE: control-freec/11.5

---
+++ CBI/control-freec/config.mk
@@ -0,0 +1,8 @@
+NAME=FREEC
+VERSION:=11.5
+MODULE_NAME=control-freec
+URL=https://github.com/BoevaLab/FREEC
+DOWNLOAD_TARGET_FILE=src/freec
+CONFIG=false
+BUILD_TARGET_FILE=bin/freec
+INSTALL_TARGET_FILE=bin/freec

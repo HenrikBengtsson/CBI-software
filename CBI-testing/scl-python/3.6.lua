e809d64c25cc72c8f956c85d313aea94c204fb8f
hb <hb@aroma-project.org>
2020-05-30 12:54:56 -0700

    scl-devtools graduated to CBI (from CBI-testing)

--- CBI-testing/scl-python/3.6.lua
+++ CBI-testing/scl-python/3.6.lua
@@ -13,7 +13,7 @@ end
 whatis("Version: " .. version)
 whatis("Keywords: programming, Python")
 whatis("URL: https://www.softwarecollections.org/en/scls/rhscl/" .. scl_name .. "/")
-whatis("Description: [DEPRECATED: Python SCLs are deprecated on Wynton HPC because `python3` is now available everywhere.]  Enables the CentOS Software Collection (SCL) `" .. scl_name .. "` in the current environment.  This is an alternative to calling `source scl_source enable " .. scl_name .. "`, which is not officially supported by RedHat/CentOS.  Example: `python --version` and `pip --version`.")
+whatis("Description: Enables the CentOS Software Collection (SCL) `" .. scl_name .. "` in the current environment.  This is an alternative to calling `source scl_source enable " .. scl_name .. "`, which is not officially supported by RedHat/CentOS.  Example: `python --version` and `pip --version`.  Warning: This module is DEPRECATED and should no longer be used because Python SCLs are deprecated, which in turn is because Python 3 is now available directly by CentOS.")
 
 
 require "posix"
@@ -32,4 +32,3 @@ end
 local path = dirname(myFileName())
 local pathname = pathJoin(path, ".incl", version_sans_period .. ".lua")
 loadfile(pathname)()
-

bf3fd9ed7f8cd9609c5e5968e06ad090b165b9d9
hb <hb@aroma-project.org>
2020-09-19 14:47:43 -0700

    bashdb/4.4-1.0.1

---
+++ CBI/bashdb/config.mk
@@ -0,0 +1,7 @@
+NAME=bashdb
+VERSION:=4.4-1.0.1
+URL=http://bashdb.sourceforge.net/
+DOWNLOAD_TARGET_FILE=configure
+#CONFIG_OPTS=--with-bash-src=...
+BUILD_TARGET_FILE=bashdb
+INSTALL_TARGET_FILE=bashdb

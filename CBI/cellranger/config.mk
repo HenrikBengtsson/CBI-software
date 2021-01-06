d75d5c25473e712a40cd2db553c124da3b9a8fed
Henrik Bengtsson <hb@aroma-project.org>
2021-01-05 17:22:00 -0800

    cellranger/5.0.1

--- CBI/cellranger/config.mk
+++ CBI/cellranger/config.mk
@@ -1,5 +1,5 @@
 NAME=cellranger
-VERSION:=4.0.0
+VERSION:=5.0.1
 URL=https://support.10xgenomics.com/single-cell-gene-expression/software/downloads/latest
 DOWNLOAD_TARGET_FILE=cellranger-shell
 CONFIG=false

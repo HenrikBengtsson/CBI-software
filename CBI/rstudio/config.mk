9b01bb2b116b418274a3d83314d4d7aad65a27da
hb <hb@aroma-project.org>
2021-02-05 09:50:37 -0800

    BUG FIX: 'config' for rstudio now checks for and reports on missing libraries

--- CBI/rstudio/config.mk
+++ CBI/rstudio/config.mk
@@ -1,7 +1,7 @@
 NAME=rstudio
 VERSION:=1.4.1103
 URL=https://rstudio.com/products/rstudio/#rstudio-desktop
-DOWNLOAD_TARGET_FILE=VERSION
-CONFIG_TARGET_FILE=bin/rstudio
+DOWNLOAD_TARGET_FILE=bin/rstudio
+CONFIG_TARGET_FILE=ldd-checks.tmp
 BUILD=false
 INSTALL_TARGET_FILE=bin/rstudio

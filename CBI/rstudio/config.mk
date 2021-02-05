550f8742d9a5bdc8dc399743549982313288f1a5
Henrik Bengtsson <hb@aroma-project.org>
2021-02-05 09:32:53 -0800

    Add rstudio/1.4.1103

---
+++ CBI/rstudio/config.mk
@@ -0,0 +1,7 @@
+NAME=rstudio
+VERSION:=1.4.1103
+URL=https://rstudio.com/products/rstudio/#rstudio-desktop
+DOWNLOAD_TARGET_FILE=VERSION
+CONFIG_TARGET_FILE=bin/rstudio
+BUILD=false
+INSTALL_TARGET_FILE=bin/rstudio

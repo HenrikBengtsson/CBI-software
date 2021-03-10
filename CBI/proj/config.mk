77f554ddfd0eb6de52c4f331b1cd744c96b66bc1
Henrik Bengtsson <hb@aroma-project.org>
2021-03-09 20:43:09 -0800

    proj/4.9.3

---
+++ CBI/proj/config.mk
@@ -0,0 +1,13 @@
+NAME=proj
+VERSION=7.2.1
+
+URL_DOWNLOAD=https://proj.org/download.html
+DOWNLOAD_FILE=configure
+BUILD_TARGET_FILE=libtool
+INSTALL_TARGET_FILE=bin/proj
+
+## PROJ 7.2.1 requires sqlite3 (>= 3.11)
+CONFIG_MODULES=CBI sqlite
+
+## PROJ 4.9.3 requires sqlite3 (>= 3.??)
+#CONFIG_MODULES=CBI sqlite

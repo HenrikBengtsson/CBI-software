92473975bfa8b047422c6ae4227e49ca6e0374f0
Henrik Bengtsson <hb@aroma-project.org>
2021-05-25 09:20:46 -0700

    tig/2.5.3: add

--- CBI/r/config.mk
+++ CBI/r/config.mk
@@ -2,8 +2,10 @@ NAME=R
 VERSION:=4.0.5
 URL_DOWNLOAD=https://cran.r-project.org/
 URL=https://cran.r-project.org/src/base/R-4/
-CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib
+CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib --without-recommended-packages
 BUILD_TARGET_FILE=bin/exec/R
 BUILD_OPTS=-j 4
 INSTALL_TARGET_FILE=bin/R
 MODULE_NAME=r
+CUSTOM_GCC=true
+
---
+++ CBI/tig/config.mk
@@ -0,0 +1,7 @@
+NAME=tig
+VERSION:=2.5.3
+URL=https://github.com/jonas/tig
+DOWNLOAD_TARGET_FILE=configure
+BUILD_TARGET_FILE=src/tig
+INSTALL_TARGET_FILE=bin/tig
+

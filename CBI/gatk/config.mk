88485abae17cff7f3d1d4a5839dd624fdc541b87
Henrik Bengtsson <hb@aroma-project.org>
2021-06-01 12:30:28 -0700

    gatk/4.2.0.0

--- CBI/gatk/config.mk
+++ CBI/gatk/config.mk
@@ -1,5 +1,5 @@
 NAME=gatk
-VERSION:=4.1.9.0
+VERSION:=4.2.0.0
 URL_DOWNLOAD=https://github.com/broadinstitute/gatk/releases
 URL=https://software.broadinstitute.org/gatk/
 DOWNLOAD_TARGET_FILE=$(NAME)

b1d24d1a6a2f686d663e357989208c602b7f2fec
Henrik Bengtsson <hb@aroma-project.org>
2021-06-01 12:49:32 -0700

    mutect: use original mixed-case install paths

--- CBI/mutect/config.mk
+++ CBI/mutect/config.mk
@@ -1,9 +1,9 @@
-NAME=mutect
+NAME=muTect
 VERSION:=1.1.5
 URL_DOWNLOAD=https://github.com/broadinstitute/mutect
 URL=https://software.broadinstitute.org/cancer/cga/mutect
-NAME2=muTect
-DOWNLOAD_TARGET_FILE=$(NAME2)-$(VERSION).jar
+NAME2=mutect
+DOWNLOAD_TARGET_FILE=$(NAME)-$(VERSION).jar
 CONFIG=false
 BUILD=false
-INSTALL_TARGET_FILE=$(NAME2)-$(VERSION).jar
+INSTALL_TARGET_FILE=$(NAME)-$(VERSION).jar

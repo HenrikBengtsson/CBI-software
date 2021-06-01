8c04e4720f6784d345db56f7893707c6618bd95d
Henrik Bengtsson <hb@aroma-project.org>
2021-05-31 17:00:49 -0700

    r/4.1.0: preparing for building with specific GCC version

--- CBI/r/config.mk
+++ CBI/r/config.mk
@@ -1,5 +1,5 @@
 NAME=R
-VERSION:=4.0.5
+VERSION:=4.1.0
 URL_DOWNLOAD=https://cran.r-project.org/
 URL=https://cran.r-project.org/src/base/R-4/
 CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib --without-recommended-packages
@@ -7,5 +7,4 @@ BUILD_TARGET_FILE=bin/exec/R
 BUILD_OPTS=-j 4
 INSTALL_TARGET_FILE=bin/R
 MODULE_NAME=r
-CUSTOM_GCC=true
-
+GCC_VERSION=7

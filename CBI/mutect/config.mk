6d812fe1bf675294828f9168fc35bce1b24b0f4e
Henrik Bengtsson <hb@aroma-project.org>
2021-06-01 12:30:53 -0700

    mutect/1.1.5

---
+++ CBI/mutect/config.mk
@@ -0,0 +1,9 @@
+NAME=mutect
+VERSION:=1.1.5
+URL_DOWNLOAD=https://github.com/broadinstitute/mutect
+URL=https://software.broadinstitute.org/cancer/cga/mutect
+NAME2=muTect
+DOWNLOAD_TARGET_FILE=$(NAME2)-$(VERSION).jar
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=$(NAME2)-$(VERSION).jar

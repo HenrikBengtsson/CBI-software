7dd95c873324c9d05367eb6174d7718d68079753
Henrik Bengtsson <hb@aroma-project.org>
2021-09-29 16:35:36 -0700

    htstools/0.1.1

---
+++ CBI/htstools/config.mk
@@ -0,0 +1,7 @@
+NAME=htstools
+VERSION:=0.1.1
+URL_DOWNLOAD=https://github.com/mskcc/htstools/
+DOWNLOAD_TARGET_FILE=snp-pileup.cpp
+CONFIG=false
+BUILD_TARGET_FILE=snp-pileup
+INSTALL_TARGET_FILE=snp-pileup

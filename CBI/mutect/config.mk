fd5e7cac4b71fd7765da72f625da16546e8d981b
Henrik Bengtsson <hb@aroma-project.org>
2021-10-02 10:30:12 -0700

    BUG FIX: mutect installed to camelCase folder. Add support for install muTect 1.1.1 and 1.1.4

--- CBI/mutect/config.mk
+++ CBI/mutect/config.mk
@@ -1,8 +1,7 @@
-NAME=muTect
+NAME=mutect
 VERSION:=1.1.5
 URL_DOWNLOAD=https://github.com/broadinstitute/mutect
 URL=https://software.broadinstitute.org/cancer/cga/mutect
-NAME2=mutect
 DOWNLOAD_TARGET_FILE=$(NAME)-$(VERSION).jar
 CONFIG=false
 BUILD=false

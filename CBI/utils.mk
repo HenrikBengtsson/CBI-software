ff1dd828833291ac70e2eed902b7c1f9e5f307c6
Henrik Bengtsson <hb@aroma-project.org>
2021-10-08 16:25:38 -0700

    Infer VERSION_Y (sic!) from VERSION_X_Y

--- CBI/utils.mk
+++ CBI/utils.mk
@@ -16,6 +16,7 @@ $(eval REMAINDER2 := $$$(REMAINDERtmp))
 VERSION_X_Y_Z := $(shell echo "$(VERSION)" | sed -E 's/([[:digit:]]+)([.-])([[:digit:]]+)([.-])([[:digit:]]+).*/\1\2\3\4\5/')
 VERSION_X_Y := $(shell echo "$(VERSION_X_Y_Z)" | sed -E 's/[.-][^.-]*$$//')
 VERSION_X := $(shell echo "$(VERSION_X_Y)" | sed -E 's/[.-][^.-]*$$//')
+VERSION_Y := $(shell echo "$(VERSION_X_Y)" | sed -E 's/^[^.-]*[.-]//')
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## CORE
@@ -254,6 +255,7 @@ debug:
 	@echo "VERSION_X_Y_Z: $(VERSION_X_Y_Z)"
 	@echo "VERSION_X_Y: $(VERSION_X_Y)"
 	@echo "VERSION_X: $(VERSION_X)"
+	@echo "VERSION_Y: $(VERSION_Y)"
 	@echo "URL: $(URL)"
 	@echo
 	@echo "CORE:"

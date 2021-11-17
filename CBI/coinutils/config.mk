c29411beed8d326047185c4d987637dd4dfcca55
Henrik Bengtsson <hb@aroma-project.org>
2021-11-16 22:29:58 -0800

    coinutils/2.11.4

---
+++ CBI/coinutils/config.mk
@@ -0,0 +1,8 @@
+NAME=CoinUtils
+VERSION=2.11.4
+URL_DOWNLOAD=https://github.com/coin-or/CoinUtils/releases
+DOWNLOAD_FILE=configure
+BUILD_TARGET_FILE=CoinUtils/src/libCoinUtils.la
+INSTALL_TARGET_FILE=lib/libCoinUtils.so
+
+

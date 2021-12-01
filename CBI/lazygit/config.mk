0fdc89c73aae0e2a64805379a0b815b108c41f48
Henrik Bengtsson <hb@aroma-project.org>
2021-11-30 19:35:13 -0800

    lazygit/0.31.4

---
+++ CBI/lazygit/config.mk
@@ -0,0 +1,8 @@
+NAME=pandoc
+VERSION:=2.16.2
+URL_DOWNLOAD=https://github.com/jgm/pandoc/releases
+URL=https://github.com/jgm/pandoc/
+DOWNLOAD_TARGET_FILE=bin/pandoc
+CONFIG=false
+BUILD=false
+INSTALL_TARGET_FILE=bin/pandoc

bdef71bbce30af28a650820477f0a35aefd7ff94
Henrik Bengtsson <hb@aroma-project.org>
2022-01-20 10:14:10 -0800

    bowtie2/2.4.5

--- CBI/bowtie2/config.mk
+++ CBI/bowtie2/config.mk
@@ -1,5 +1,5 @@
 NAME=bowtie2
-VERSION:=2.4.4
+VERSION:=2.4.5
 URL_DOWNLOAD=https://sourceforge.net/projects/bowtie-bio/files/bowtie2/
 URL=https://github.com/BenLangmead/bowtie2/releases/
 DOWNLOAD_TARGET_FILE=bowtie2

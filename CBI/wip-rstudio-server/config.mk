8c233fd1ed618cd2fb9a439e9ab13395648b29f7
Henrik Bengtsson <hb@aroma-project.org>
2022-01-20 19:11:14 -0800

    WIP: RStudio Server

---
+++ CBI/wip-rstudio-server/config.mk
@@ -0,0 +1,7 @@
+NAME=rstudio-server
+VERSION:=2021.09.2-382
+URL_DOWNLOAD=https://github.com/rstudio/rstudio/tags
+DOWNLOAD_TARGET_FILE=INSTALL
+CONFIG=false
+BUILD_TARGET_FILE=foo
+INSTALL_TARGET_FILE=bin/rstudio

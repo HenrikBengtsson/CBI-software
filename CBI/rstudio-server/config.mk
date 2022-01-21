c9a8fb664de47c4af0ac6dc9e3fc85a7c90ff321
Henrik Bengtsson <hb@aroma-project.org>
2022-01-20 23:02:39 -0800

    rstudio/2021.09.2-382: a first ever install from source - not sure if it works

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -3,5 +3,6 @@ VERSION:=2021.09.2-382
 URL_DOWNLOAD=https://github.com/rstudio/rstudio/tags
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
-BUILD_TARGET_FILE=build/foo
-INSTALL_TARGET_FILE=bin/rstudio
+BUILD_TARGET_FILE=build/CMakeFiles/Makefile.cmake
+INSTALL_TARGET_FILE=bin/rserver
+

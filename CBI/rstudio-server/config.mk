6a4847432b75293e6e0f0d42ee9994461579ebd8
Henrik Bengtsson <hb@aroma-project.org>
2022-01-21 20:18:31 -0800

    make build: now using a valid target

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -3,6 +3,6 @@ VERSION:=2021.09.2-382
 URL_DOWNLOAD=https://github.com/rstudio/rstudio/tags
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
-BUILD_TARGET_FILE=build/CMakeFiles/Makefile.cmake
+BUILD_TARGET_FILE=build/src/cpp/server/crash-handler-proxy/crash-handler-proxy
 INSTALL_TARGET_FILE=bin/rserver
 

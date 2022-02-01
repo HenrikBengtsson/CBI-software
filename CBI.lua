320727adca15527131a50435312360ed1022529e
Henrik Bengtsson <hb@aroma-project.org>
2022-02-01 14:06:53 -0800

    Make super module 'CBI' know about C4 too

--- CBI.lua
+++ CBI.lua
@@ -1,12 +1,14 @@
-help([[
-The Computational Biology and Informatics (CBI) Software Repository
-]])
-whatis("Title: The Computational Biology and Informatics (CBI) Software Repository")
-whatis("Description: Repository of software shared by the Computational Biology and Informatics (http://cbi.ucsf.edu) at the UCSF Helen Diller Family Comprehensive Cancer Center.")
+help("Module Repository by Computational Biology and Informatics (CBI)")
+
+local name = myModuleName()
+
+whatis("Keywords: UCSF, CBI")
 whatis("URL: http://cbi.ucsf.edu/")
-whatis("BugReports: Please contact the maintainer of this repository")
-whatis("Maintainer: Henrik Bengtsson, Computational Biology and Informatics (http://cbi.ucsf.edu)")
-whatis("Keywords: UCSF, CBI, repository")
+whatis("Description: Repository of modules shared by Computational Biology and Informatics (http://cbi.ucsf.edu/). When loading this module, you will get access to a large number of software modules.  When unloading the module, any software modules from this " .. name .. " repository still loaded when you unload this repository module, will remain in your list of loaded modules but will be inactivate (i.e. behave as they never were loaded) until you reload this repository module again.  Example: `module load " .. name .. "` and then `module avail`.")
+
+name = string.lower(name)
+prepend_path("MODULEPATH", pathJoin("/software", "c4", name, "modulefiles"))
+setenv("SOFTWARE_ROOT_CBI", pathJoin("/software", "c4", name, "software"))
 
 require "io"
 function isdir(fn)
@@ -21,6 +23,9 @@ if isdir("/wynton/home/cbi/shared/software/CBI") then
    -- Wynton
    cbi_software_home = "/wynton/home/cbi/shared/software/CBI"
    cbi_modules_home  = "/wynton/home/cbi/shared/modulefiles/CBI"
+elseif isdir("/software/c4/cbi/software/") then
+   cbi_software_home = "/software/c4/cbi/software"
+   cbi_modules_home  = "/software/c4/cbi/modulefiles"
 elseif isdir("/home/shared/cbc/software_cbc/shared/apps/manual") then
    -- TIPCC (legacy)
    cbi_software_home = "/home/shared/cbc/software_cbc/shared/apps/manual"

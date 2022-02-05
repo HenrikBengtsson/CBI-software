1d7d5c7515a298c33c3aaf8e09d9167fd2795409
Henrik Bengtsson <hb@aroma-project.org>
2022-02-04 19:02:22 -0800

    proj: cleanup

--- CBI/proj/config.mk
+++ CBI/proj/config.mk
@@ -4,9 +4,3 @@ VERSION=6.0.0
 DOWNLOAD_FILE=configure
 BUILD_TARGET_FILE=libtool
 INSTALL_TARGET_FILE=bin/proj
-
-## PROJ 7.2.1 requires sqlite3 (>= 3.11)
-CONFIG_MODULES=CBI sqlite
-
-## PROJ 4.9.3 requires sqlite3 (>= 3.??)
-CONFIG_MODULES=

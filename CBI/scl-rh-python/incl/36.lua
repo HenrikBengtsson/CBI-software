81f0b0f5af60fb4e6131a1a8939a7e2d76915c46
hb <hb@aroma-project.org>
2022-02-08 15:40:34 -0800

    scl-rh-python: make sure that we always get prepend_path()

--- CBI/scl-rh-python/incl/36.lua
+++ CBI/scl-rh-python/incl/36.lua
@@ -3,5 +3,5 @@
 prepend_path("LD_LIBRARY_PATH","/opt/rh/rh-python36/root/usr/lib64")
 prepend_path("MANPATH","/opt/rh/rh-python36/root/usr/share/man")
 prepend_path("PATH","/opt/rh/rh-python36/root/usr/bin")
-setenv("PKG_CONFIG_PATH","/opt/rh/rh-python36/root/usr/lib64/pkgconfig")
-setenv("XDG_DATA_DIRS","/opt/rh/rh-python36/root/usr/share:/usr/local/share:/usr/share")
+prepend_path("PKG_CONFIG_PATH","/opt/rh/rh-python36/root/usr/lib64/pkgconfig")
+prepend_path("XDG_DATA_DIRS","/opt/rh/rh-python36/root/usr/share")

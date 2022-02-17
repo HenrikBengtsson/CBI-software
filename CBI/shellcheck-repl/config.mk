6ad4718ad373178f36ddfdc1d74e4033e673a5ca
Henrik Bengtsson <hb@aroma-project.org>
2022-02-17 10:30:46 -0800

    Need to manual drop leading periods from version for _hidden_ modules

--- CBI/shellcheck-repl/config.mk
+++ CBI/shellcheck-repl/config.mk
@@ -1,8 +1,8 @@
 NAME=shellcheck-repl
-VERSION:=0.1.2
+VERSION=0.1.2
 DOWNLOAD_TARGET_FILE=shellcheck-repl.bash
 CONFIG=false
 BUILD=false
 INSTALL_TARGET_FILE=shellcheck-repl.bash
--- Hidden for now /HB 2022-02-17
+## Hidden for now /HB 2022-02-17
 MODULE_HIDDEN=true

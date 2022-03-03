80d706c7e8222d52a4e6e9dad1e0fc23020d6b6d
Henrik Bengtsson <hb@aroma-project.org>
2022-03-03 12:26:53 -0800

    rstudio-server/2022.02.0-443

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -1,6 +1,5 @@
 NAME=rstudio-server
-VERSION:=2021.09.2-382
-URL_DOWNLOAD=https://github.com/rstudio/rstudio/tags
+VERSION:=2022.02.0-443
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
 BUILD_TARGET_FILE=build/src/cpp/server/crash-handler-proxy/crash-handler-proxy

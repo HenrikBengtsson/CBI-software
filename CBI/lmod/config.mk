d7887015289b463e294b5ff2e81783c45d824d1c
Henrik Bengtsson <hb@aroma-project.org>
2022-03-03 14:26:45 -0800

    lmod/8.6

---
+++ CBI/lmod/config.mk
@@ -0,0 +1,8 @@
+NAME=lmod
+VERSION=8.6
+DOWNLOAD_TARGET_FILE=configure
+CONFIG_TARGET_FILE=config.log
+##BUILD_TARGET_FILE=bin/config.log
+BUILD=false
+INSTALL_TARGET_FILE=lmod/$(VERSION)/libexec/sh_to_modulefile
+INSTALL_MODULE=false

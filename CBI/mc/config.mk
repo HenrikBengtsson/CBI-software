c769222378a869c7fb580e255d02437dc691d529
Henrik Bengtsson <hb@aroma-project.org>
2022-03-10 09:20:09 -0800

    pandoc/2.17.1.1

--- CBI/mc/config.mk
+++ CBI/mc/config.mk
@@ -1,7 +1,5 @@
 NAME=mc
 VERSION:=4.8.27
-URL_DOWNLOAD=https://github.com/MidnightCommander/mc/releases
-URL=https://github.com/MidnightCommander/mc
 CONFIG_OPTS=--with-screen=ncurses
 BUILD_TARGET_FILE=bin/mc
 BUILD_OPTS=-j 4

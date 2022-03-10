54ab2d89b31653e1a2e5289371c062969dff5835
Henrik Bengtsson <hb@aroma-project.org>
2022-03-10 08:56:38 -0800

    emacs/27.2: update URLs

--- CBI/emacs/config.mk
+++ CBI/emacs/config.mk
@@ -1,7 +1,5 @@
 NAME=emacs
 VERSION:=27.2
-URL_DOWNLOAD=http://gnu.mirrors.hoobly.com/emacs
-URL=http://gnu.mirrors.hoobly.com/emacs
 CONFIG_OPTS=--with-xpm=no --with-gif=no --with-gnutls=no
 BUILD_TARGET_FILE=src/emacs
 INSTALL_TARGET_FILE=bin/emacs

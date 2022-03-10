17e015a5bf664c390abc1656ded0cdbb1b40fada
Henrik Bengtsson <hb@aroma-project.org>
2022-03-10 09:14:12 -0800

    igv/2.12.3

--- CBI/igv/config.mk
+++ CBI/igv/config.mk
@@ -1,7 +1,5 @@
 NAME=IGV
-VERSION:=2.11.9
-URL_DOWNLOAD=https://software.broadinstitute.org/software/igv/download
-URL=https://software.broadinstitute.org/software/igv/
+VERSION:=2.12.3
 DOWNLOAD_TARGET_FILE=igv.sh
 CONFIG=false
 BUILD=false

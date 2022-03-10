07852917e39fa8304b70097a448b302a2b12a7bf
Henrik Bengtsson <hb@aroma-project.org>
2022-03-10 08:58:56 -0800

    fzf/0.29.0: update URLs

--- CBI/fzf/config.mk
+++ CBI/fzf/config.mk
@@ -1,7 +1,5 @@
 NAME=fzf
 VERSION:=0.29.0
-URL_DOWNLOAD=https://github.com/junegunn/fzf/releases
-URL=https://github.com/junegunn/fzf/
 DOWNLOAD_TARGET_FILE=install
 CONFIG=false
 BUILD_TARGET_FILE=bin/fzf

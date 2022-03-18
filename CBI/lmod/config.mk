c739e5f4fa39f6929ba5831de7837f3658728be1
hb <hb@aroma-project.org>
2022-03-18 14:51:10 -0700

    lmod: fix

--- CBI/lmod/config.mk
+++ CBI/lmod/config.mk
@@ -2,7 +2,6 @@ NAME=lmod
 VERSION=8.6
 DOWNLOAD_TARGET_FILE=configure
 CONFIG_TARGET_FILE=config.log
-##BUILD_TARGET_FILE=bin/config.log
 BUILD=false
 INSTALL_TARGET_FILE=lmod/$(VERSION)/libexec/sh_to_modulefile
 INSTALL_MODULE=false

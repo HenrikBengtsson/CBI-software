daa2ba703b8eb6675ab6de364ed6118378d85bd8
Henrik Bengtsson <hb@aroma-project.org>
2022-06-18 15:53:34 -0700

    rstudio/2022.02.3-492: update + assert correct version installed

--- CBI/rstudio/config.mk
+++ CBI/rstudio/config.mk
@@ -1,6 +1,7 @@
 NAME=rstudio
-VERSION:=2022.02.1-461
-ARCH=x86_64-fedora
+VERSION:=2022.02.3-492
+#ARCH=amd64-debian   ## Ubuntu 20.04
+ARCH=x86_64-fedora  ## Centos 7.9
 DOWNLOAD_TARGET_FILE=bin/rstudio
 CONFIG_TARGET_FILE=ldd-checks.tmp
 BUILD=false

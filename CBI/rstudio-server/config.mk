3480fe2ea986dad40f9cd366ee8e949edf37e414
Henrik Bengtsson <hb@aroma-project.org>
2022-09-29 19:18:14 -0700

    rstudio-server/2022.07.2-576: requires 'jq' to build

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -1,5 +1,5 @@
 NAME=rstudio-server
-VERSION:=2022.02.1-461
+VERSION:=2022.07.2-576
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
 BUILD_TARGET_FILE=build/src/cpp/server/crash-handler-proxy/crash-handler-proxy

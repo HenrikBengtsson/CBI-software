aafe812f7779283e41ae8f37e6dcb912cfe95597
Henrik Bengtsson <hb@aroma-project.org>
2022-12-15 20:46:11 -0800

    rstudio/2022.12.0-353

--- CBI/rstudio/config.mk
+++ CBI/rstudio/config.mk
@@ -1,10 +1,10 @@
 NAME=rstudio
-VERSION:=2022.07.2-576
+VERSION=2022.12.0-353
 ## Ubuntu 20.04:
 #ARCH=amd64-debian
 ## Centos 7.9:
 ARCH=x86_64-fedora
-DOWNLOAD_TARGET_FILE=bin/rstudio
+DOWNLOAD_TARGET_FILE=rstudio
 CONFIG_TARGET_FILE=ldd-checks.tmp
 BUILD=false
-INSTALL_TARGET_FILE=bin/rstudio
+INSTALL_TARGET_FILE=rstudio

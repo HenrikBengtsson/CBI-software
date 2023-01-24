d01103bcdb5f20cef00336b1a9e010da623bbf96
Henrik Bengtsson <hb@aroma-project.org>
2023-01-23 22:07:03 -0800

    Add more 'make available-version'

--- CBI/bashdb/config.mk
+++ CBI/bashdb/config.mk
@@ -1,5 +1,5 @@
 NAME=bashdb
-VERSION:=4.4-1.0.1
+VERSION=4.4-1.0.1
 URL_DOWNLOAD=https://sourceforge.net/projects/bashdb/files/bashdb/
 URL=http://bashdb.sourceforge.net/
 DOWNLOAD_TARGET_FILE=configure

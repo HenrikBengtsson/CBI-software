592b608130cc4b0fe523602e4f82e084ebd07624
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-02-13 12:17:26 -0800

    restic/0.15.1

--- CBI/shellcheck-repl/config.mk
+++ CBI/shellcheck-repl/config.mk
@@ -1,8 +1,7 @@
 NAME=shellcheck-repl
-VERSION=0.1.3
+VERSION=0.4.0
 DOWNLOAD_TARGET_FILE=shellcheck-repl.bash
 CONFIG=false
 BUILD=false
 INSTALL_TARGET_FILE=shellcheck-repl.bash
-## Hidden for now /HB 2022-02-17
-MODULE_HIDDEN=true
+

a9ce6e042a0b41889a433d473038d4dbc46d5677
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-25 16:35:48 -0700

    rstudio/2023.03.0+386

--- CBI/rstudio/config.mk
+++ CBI/rstudio/config.mk
@@ -1,9 +1,7 @@
 NAME=rstudio
 VERSION=2022.12.0-353
-## Ubuntu 20.04:
-#ARCH=amd64-debian
-## Centos 7.9:
-ARCH=x86_64-fedora
+VERSION=2023.03.0+386
+
 DOWNLOAD_TARGET_FILE=rstudio
 CONFIG_TARGET_FILE=ldd-checks.tmp
 BUILD=false

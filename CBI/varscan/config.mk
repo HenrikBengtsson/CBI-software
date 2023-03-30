13e0d37616e902df95f92b2f30c6dbb85711547b
Henrik Bengtsson <hb@aroma-project.org>
2023-03-30 09:29:28 -0700

    Make a note about VERSION >= 2.4.5 not working

--- CBI/varscan/config.mk
+++ CBI/varscan/config.mk
@@ -1,4 +1,5 @@
 NAME=VarScan
+## VERSION >= 2.4.5 gives Error: Invalid or corrupt jarfile /software/c4/cbi/software/VarScan-2.4.6/VarScan.jar
 VERSION=2.4.2
 DOWNLOAD_TARGET_FILE=$(NAME).v$(VERSION).jar
 CONFIG=false

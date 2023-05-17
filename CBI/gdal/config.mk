98c1d1bc7b0ea3b69ad71fa838054bb4cebf09d4
Henrik Bengtsson <hb@aroma-project.org>
2023-05-16 19:41:14 -0700

    gdal/4.6.3: using a modern SQLite3, proj/8.2.1 [#86]

--- CBI/gdal/config.mk
+++ CBI/gdal/config.mk
@@ -1,8 +1,19 @@
 NAME=gdal
+
+## GDAL < 3.6.0
 VERSION=2.4.4
 DOWNLOAD_FILE=configure
 BUILD_TARGET_FILE=apps/gdalinfo
+
+## GDAL >= 3.6.0
+VERSION=3.6.4
+DOWNLOAD_TARGET_FILE=gdal.cmake
+CONFIG_TARGET_FILE=build/Makefile
+BUILD_TARGET_FILE=build/apps/gdalinfo
+
+## All versions
 INSTALL_TARGET_FILE=bin/gdalinfo
+
 
 
 

45df6f8b8a76b3b6d96cee900ec9038427c11f9a
hb <hb@aroma-project.org>
2023-06-14 04:08:39 -0700

    cluster-utils/23.04.1: does not work; let's wait for the next version

--- CBI/cluster-utils/config.mk
+++ CBI/cluster-utils/config.mk
@@ -1,4 +1,13 @@
 NAME=cluster-utils
+
+## 'ctop -V' for VERSION=23.04.1 gives:
+## Can't locate Curses.pm in @INC (@INC contains: /usr/local/lib64/perl5 /usr/local/share/perl5
+## /usr/lib64/perl5/vendor_perl /usr/share/perl5/vendor_perl /usr/lib64/perl5 /usr/share/perl5 .)
+## at /wynton/home/cbi/shared/software/CBI/cluster-utils-23.04.1/bin/ctop line 71.
+## BEGIN failed--compilation aborted at
+## /wynton/home/cbi/shared/software/CBI/cluster-utils-23.04.1/bin/ctop line 71.
+## VERSION=23.04.1
+
 VERSION=23.03.1
 URL_DOWNLOAD=https://github.com/molgenis/cluster-utils/releases/
 URL=https://github.com/molgenis/cluster-utils/releases/

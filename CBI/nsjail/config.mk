ec0feb4a456c347e65da6fc946461310fe43a9e8
hb <hb@aroma-project.org>
2023-07-05 07:54:59 -0700

    nsjail: requires CGroups v2, which is _not_ supported by CentOS 7

--- CBI/nsjail/config.mk
+++ CBI/nsjail/config.mk
@@ -1,6 +1,10 @@
+## nsjail requires CGroups v2, which is _not_ supported by CentOS 7
+
 NAME=nsjail
 VERSION=3.3
 DOWNLOAD_TARGET_FILE=Makefile
-CONFIG=false
+CONFIG_TARGET_FILE=Makefile
 BUILD_TARGET_FILE=nsjail
 INSTALL_TARGET_FILE=nsjail
+
+BUILD_MODULES=CBI scl-devtoolset/10

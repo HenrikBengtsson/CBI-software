5342ce473c7b6d15cd50edca825fc37cb647a186
hb <hb@aroma-project.org>
2023-09-07 11:57:17 -0700

    tophat: Patch python -> python2 by adding a build step => works also on Rocky 8 [#93]

--- CBI/tophat/config.mk
+++ CBI/tophat/config.mk
@@ -3,5 +3,5 @@ VERSION:=2.1.1
 #VERSION:=2.1.2 ## Only available on GitHub and doesn't install
 DOWNLOAD_TARGET_FILE=tophat
 CONFIG=false
-BUILD=false
+BUILD_TARGET_FILE=.patched/done
 INSTALL_TARGET_FILE=tophat

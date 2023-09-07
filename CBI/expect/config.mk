c7ca202428f27f0d723875b24d6a6a213a91ff59
hb <hb@aroma-project.org>
2023-09-06 22:12:12 -0700

    expect: distro specific

--- CBI/expect/config.mk
+++ CBI/expect/config.mk
@@ -1,5 +1,9 @@
 NAME=expect
 VERSION=5.45.4
+
+## This software needs to be built for each Linux distribution separately
+LINUX_DISTRO_SPECIFIC=true
+
 DOWNLOAD_TARGET_FILE=configure
 CONFIG=true
 BUILD_TARGET_FILE=expect

5421484cd2bd2b0d1dddaae8bba85333e651c016
hb <hb@aroma-project.org>
2023-09-07 21:15:14 -0700

    r: post_build now reports that it runs 'make check' of R

--- CBI/gistic2/config.mk
+++ CBI/gistic2/config.mk
@@ -2,7 +2,7 @@ NAME=gistic2
 VERSION:=2.0.23
 
 ## This software needs to be built for each Linux distribution separately
-LINUX_DISTRO_SPECIFIC=true
+#LINUX_DISTRO_SPECIFIC=true
 
 DOWNLOAD_TARGET_FILE=gistic2
 CONFIG=false

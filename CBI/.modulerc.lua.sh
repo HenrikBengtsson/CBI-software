7339ae4e1680ec7dcc183d1d2f91c6c898665fd5
hb <hb@aroma-project.org>
2023-09-11 14:03:01 -0700

    modulerc: hide also modules that are symbolic links

--- CBI/.modulerc.lua.sh
+++ CBI/.modulerc.lua.sh
@@ -12,7 +12,7 @@ echo ""
 ## Hide too specific R versions
 echo "-- Hide R module versions that include GCC version;"
 echo "-- it's suffient expose their shorter aliases"
-mapfile -t versions < <(find "${MODULE_HOME}/r" -type f -name '*gcc*' -printf '%f\n' | sed -E 's/[.]lua//' | grep -v -E "^[.]")
+mapfile -t versions < <(find "${MODULE_HOME}/r" \( -type f -o -type l \) -name '*gcc*' -printf '%f\n' | sed -E 's/[.]lua//' | grep -v -E "^[.]")
 for version in "${versions[@]}"; do
     printf "hide_version(\"r/%s\")\n" "${version}"
 done
@@ -20,7 +20,7 @@ echo
 
 ## Hide "false" modules specific Linux distros
 echo "-- Hide modules in folders specific to Linux distributions"
-mapfile -t modules < <(cd "${MODULE_HOME}"; find . -type f -name '*.lua' -print | sed -E 's/(^[.]\/|[.]lua$)//g' | grep -E "^_")
+mapfile -t modules < <(cd "${MODULE_HOME}"; find . \( -type f -o -type l \) -name '*.lua' -print | sed -E 's/(^[.]\/|[.]lua$)//g' | grep -E "^_")
 for module in "${modules[@]}"; do
     printf "hide_version(\"%s\")\n" "${module}"
 done

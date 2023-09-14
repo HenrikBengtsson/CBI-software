d1c4299393d3f70e390d001005a796c25a9e97b5
hb <hb@aroma-project.org>
2023-09-13 18:41:12 -0700

    cutadapt: attempt to set up automatic installation of cutadapt; it's not easy and it still doesn't work [#102]

--- CBI/cutadapt/config.mk
+++ CBI/cutadapt/config.mk
@@ -2,11 +2,11 @@ NAME=cutadapt
 ## VERSION=4.3 is not available
 VERSION=4.4
 
-## This software needs to be built for each Linux distribution separately
-LINUX_DISTRO_SPECIFIC=true
-
 NAME2=trimgalore
 DOWNLOAD=false
 CONFIG=false
 BUILD=false
 INSTALL_TARGET_FILE=bin/cutadapt
+
+## This software needs to be built for each Linux distribution separately
+LINUX_DISTRO_SPECIFIC=true

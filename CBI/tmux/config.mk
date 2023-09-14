7943c4700893579ec2336a5b460629bf93d7533d
hb <hb@aroma-project.org>
2023-09-14 16:45:43 -0700

    tmux: With 'ncurses-compat-libs' installed on Rocky 8, this software does no longer have to be installed separately from CentOS 7 and Rocky 8

--- CBI/tmux/config.mk
+++ CBI/tmux/config.mk
@@ -1,8 +1,5 @@
 NAME=tmux
-VERSION:=2.8
-
-## This software needs to be built for each Linux distribution separately
-LINUX_DISTRO_SPECIFIC=true
+VERSION=2.8
 
 BUILD_TARGET_FILE=tmux
 INSTALL_TARGET_FILE=bin/tmux

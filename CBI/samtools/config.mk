06bf597b143a0f6622c4424b0dc99706ae5594bc
hb <hb@aroma-project.org>
2023-09-14 17:02:03 -0700

    samtools: With 'ncurses-compat-libs' installed on Rocky 8, this software does no longer have to be installed separately from CentOS 7 and Rocky 8

--- CBI/samtools/config.mk
+++ CBI/samtools/config.mk
@@ -1,8 +1,5 @@
 NAME=samtools
 VERSION=1.18
 
-## This software needs to be built for each Linux distribution separately
-LINUX_DISTRO_SPECIFIC=true
-
 BUILD_TARGET_FILE=$(NAME)
 INSTALL_TARGET_FILE=bin/$(NAME)

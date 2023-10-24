f6ded0e14153edd95fb0474c4f1246a26bc1c47a
hb <hb@aroma-project.org>
2023-10-24 15:21:02 -0700

    htop is now distro specific, so that it can run also on login and data-trasfer nodes, which lacks 'ncurses-compat-libs'

--- CBI/htop/config.mk
+++ CBI/htop/config.mk
@@ -1,5 +1,9 @@
 NAME=htop
 VERSION=3.2.2
 
+## This software needs to be built for each Linux distribution separately
+## (unless 'ncurses-compat-libs' is installed)
+LINUX_DISTRO_SPECIFIC=true
+
 BUILD_TARGET_FILE=htop
 INSTALL_TARGET_FILE=bin/htop

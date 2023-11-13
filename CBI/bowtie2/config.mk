5a5f93e975b39da83d4847f153ab9495a434e7c3
hb <hb@aroma-project.org>
2023-11-12 16:04:49 -0800

    bowtie2/2.5.2

--- CBI/bowtie2/config.mk
+++ CBI/bowtie2/config.mk
@@ -1,5 +1,5 @@
 NAME=bowtie2
-VERSION=2.5.1
+VERSION=2.5.2
 URL_DOWNLOAD=https://sourceforge.net/projects/bowtie-bio/files/bowtie2/
 URL=https://github.com/BenLangmead/bowtie2/releases/
 DOWNLOAD_TARGET_FILE=bowtie2

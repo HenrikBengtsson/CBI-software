738319bb4b6cebff730bee7f91665b00667a7d31
hb <hb@aroma-project.org>
2024-04-12 16:37:36 -0700

    snpeff/5.2c

--- CBI/snpeff/config.mk
+++ CBI/snpeff/config.mk
@@ -1,6 +1,6 @@
 NAME=snpEff
 ## SnpEff (>= 5.1) requires Java 12. Wynton has openjdk/17, C4 has openjdk/11
-VERSION=5.2a
+VERSION=5.2c
 DOWNLOAD_TARGET_FILE=snpEff/snpEff.jar
 CONFIG=false
 BUILD=false

5dfe39a2136cdfb9d24e77628e9a72ac03bf3436
hb <henrik.bengtsson@gmail.com>
2024-11-05 10:30:01 -0800

    rstudio-server/2024.09.1-394

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -5,7 +5,7 @@ NAME=rstudio-server
 ## Increasing the timeout can help. To do that set:
 ## $ yarn config set network-timeout 1000000
 
-VERSION=2024.09.0-375
+VERSION=2024.09.1-394
 
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile

fad48588f15402e402b40f487215cff4faab107a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-05-16 12:52:08 -0700

    rstudio-server/2025.05.0-496

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -5,11 +5,13 @@ NAME=rstudio-server
 ## Increasing the timeout can help. To do that set:
 ## $ yarn config set network-timeout 1000000
 
-VERSION=2024.12.1-563
+VERSION=2025.05.0-496
 
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
 BUILD_TARGET_FILE=build/src/cpp/server/crash-handler-proxy/crash-handler-proxy
 INSTALL_TARGET_FILE=bin/rserver
-CONFIG_MODULES=CBI cmake/3.5 jq scl-devtoolset r
 
+## Rocky 8.10 comes with cmake 3.26.5. Ubuntu 22.04 comes with cmake 3.22.1.
+## Both are sufficient for installing v2025.05.0-496 /HB 2025-05-16
+CONFIG_MODULES=CBI jq scl-devtoolset r

0f0ad37e8acf0d675caf123a95f4fe22d5d3305c
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-11-02 10:45:48 -0800

    proj/9.7.0

--- CBI/proj/config.mk
+++ CBI/proj/config.mk
@@ -1,6 +1,17 @@
 NAME=proj
-VERSION=8.2.1
 
-DOWNLOAD_FILE=configure
+## proj (>= 9) don't use configure
+VERSION=9.7.0
+include ../version.mk
+
+DOWNLOAD_TARGET_FILE=ChangeLog
+
+ifeq ($(VERSION_X),9)
+CONFIG_TARGET_FILE=build/Makefile
+BUILD_TARGET_FILE=build/bin/proj
+else
+CONFIG_TARGET_FILE=libtool
 BUILD_TARGET_FILE=libtool
+endif
+
 INSTALL_TARGET_FILE=bin/proj

b7a0bea07026bd32f5567803f0a35b91a31cf38b
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-11-02 11:34:01 -0800

    proj: Make it possible to still install older versions, e.g. VERSION=8.2.1

--- CBI/proj/config.mk
+++ CBI/proj/config.mk
@@ -1,16 +1,16 @@
 NAME=proj
 
-## proj (>= 9) don't use configure
+## PROJ 9.0.0 was released on 2022-03-01
 VERSION=9.7.0
-include ../version.mk
 
 DOWNLOAD_TARGET_FILE=ChangeLog
 
+include ../version.mk
 ifeq ($(VERSION_X),9)
 CONFIG_TARGET_FILE=build/Makefile
 BUILD_TARGET_FILE=build/bin/proj
 else
-CONFIG_TARGET_FILE=libtool
+CONFIG_TARGET_FILE=config.log
 BUILD_TARGET_FILE=libtool
 endif
 

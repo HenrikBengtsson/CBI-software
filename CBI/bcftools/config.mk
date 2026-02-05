c0cc8d892f321b1f2a789756c038fa3edbfb44f5
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-02-05 12:38:31 -0800

    bcftools/1.22

--- CBI/bcftools/config.mk
+++ CBI/bcftools/config.mk
@@ -1,4 +1,7 @@
 NAME=bcftools
-VERSION=1.23
+VERSION=1.22
 BUILD_TARGET_FILE=.patched/done
 INSTALL_TARGET_FILE=bin/$(NAME)
+
+CONFIG_MODULES=CBI scl-gcc-toolset
+DEPENDS_ON=bzip2-devel libcurl-devel

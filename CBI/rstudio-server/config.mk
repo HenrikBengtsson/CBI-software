0c21cd900abbf6953d157bb6a9a6a50af5a27754
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-02-06 17:48:25 -0500

    rstudio-server/2025.05.1-513: failing to build because gcc15 is too new

--- CBI/rstudio-server/config.mk
+++ CBI/rstudio-server/config.mk
@@ -7,6 +7,10 @@ NAME=rstudio-server
 
 VERSION=2025.09.1-401
 
+# last version that install out of the box for now
+VERSION=2025.05.1-513
+
+
 DOWNLOAD_TARGET_FILE=INSTALL
 CONFIG_TARGET_FILE=build/Makefile
 BUILD_TARGET_FILE=build/src/cpp/server/crash-handler-proxy/crash-handler-proxy
@@ -14,4 +18,4 @@ INSTALL_TARGET_FILE=bin/rserver
 
 ## Rocky 8.10 comes with cmake 3.26.5. Ubuntu 22.04 comes with cmake 3.22.1.
 ## Both are sufficient for installing v2025.05.0-496 /HB 2025-05-16
-CONFIG_MODULES=CBI jq scl-devtoolset r
+CONFIG_MODULES=CBI jq scl-devtoolset r cmake/3

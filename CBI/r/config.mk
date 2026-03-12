caacd1bb8f595f0314604536826833b487bab07d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-03-12 16:00:57 -0700

    r/4.5.3

--- CBI/r/config.mk
+++ CBI/r/config.mk
@@ -1,17 +1,17 @@
 NAME=R
-VERSION=4.5.2
+VERSION=4.5.3
 
 ## This software needs to be built for each Linux distribution separately
 LINUX_DISTRO_SPECIFIC=true
 
 CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib --without-recommended-packages
-CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib --without-recommended-packages --with-x=no
+#CONFIG_OPTS=--enable-memory-profiling --enable-R-shlib --without-recommended-packages --with-x=no
 BUILD_TARGET_FILE=bin/exec/R
 BUILD_OPTS=-j 4
 INSTALL_TARGET_FILE=bin/R
 MODULE_NAME=r
 OPENJDK_VERSION=1.8.0
-OPENJDK_VERSION=25
+#OPENJDK_VERSION=25
 
 CONFIG_MODULES=CBI scl-gcc-toolset
 

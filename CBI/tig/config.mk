dbd06c77c12bc6ac9381a4a7fbea087cdf04b550
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-05-30 14:56:50 -0700

    tig/2.6.0

--- CBI/tig/config.mk
+++ CBI/tig/config.mk
@@ -1,7 +1,8 @@
 NAME=tig
-VERSION:=2.5.10
+VERSION=2.6.0
 URL=https://github.com/jonas/tig
 DOWNLOAD_TARGET_FILE=configure
 BUILD_TARGET_FILE=src/tig
 INSTALL_TARGET_FILE=bin/tig
+RECENT_ONLY=true
 

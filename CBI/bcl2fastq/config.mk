2794cf2a919361e40a853ac660327020b5b7bd42
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-09-08 13:52:59 -0700

    bcl2fastq: update around downloads and EULA. However, this software don't install on Ubuntu 24.04

--- CBI/bcl2fastq/config.mk
+++ CBI/bcl2fastq/config.mk
@@ -4,5 +4,5 @@ DOWNLOAD_TARGET_FILE=COPYRIGHT
 CONFIG_TARGET_FILE=bcl2fastq-build/Makefile
 BUILD_TARGET_FILE=bcl2fastq-build/CMakeFiles/BCL2FASTQ_OPT.dir/depend.internal
 INSTALL_TARGET_FILE=bin/bcl2fastq
-EULA_URL="https://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2-20-eula.pdf"
+EULA_URL=https://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2-20-eula.pdf
 

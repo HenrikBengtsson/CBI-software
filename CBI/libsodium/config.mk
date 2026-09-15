d7b2d768645d594bbe464d631f2d2253bf7cd3e3
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-09-15 14:22:57 -0700

    libsodium/1.0.20

---
+++ CBI/libsodium/config.mk
@@ -0,0 +1,6 @@
+NAME=libsodium
+VERSION=1.0.20
+DOWNLOAD_TARGET_FILE=configure
+CONFIG_TARGET_FILE=config.status
+BUILD_TARGET_FILE=src/libsodium/.libs/libsodium.so
+INSTALL_TARGET_FILE=lib/libsodium.so
